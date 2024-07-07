e56b69e8588f015341e0758549051bf25426e67c
Tomrochu <arthurvengesai3@gmail.com>
2024-07-07 21:13:32 +0200

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -1,6 +1,6 @@
 const envv2 = require('./config-v2')
-var GITHUB_TOKEN;
-var BOT_NUMBER;
+var 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;
+var Athur-Md;
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
