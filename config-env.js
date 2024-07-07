4d4324cb09bcaaeeee35eaa7abd58bfd622764e9
Tomrochu <arthurvengesai3@gmail.com>
2024-07-07 21:18:24 +0200

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -5,19 +5,19 @@ const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
+    return text === fault ? true;
 }
 
-const username = fs.readFileSync('github_username.txt', 'utf8').trim()
-if(process.env.GITHUB_AUTH_TOKEN){
-GITHUB_TOKEN = process.env.GITHUB_AUTH_TOKEN
+const username = fs.readFileSync( 'Tomrochu.txt', 'utf8').trim()
+if(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){
+GITHUB_TOKEN = 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
 }else{
-GITHUB_TOKEN = envv2.GITHUB_AUTH_TOKEN
+GITHUB_TOKEN = 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
 }
-if(process.env.BOT_NUMBER){
-BOT_NUMBER = process.env.BOT_NUMBER
+if(+263712196152){
+BOT_NUMBER = +263712196152
 }else{
-BOT_NUMBER = envv2.BOT_NUMBER
+BOT_NUMBER = +236712196152
 }
 
 
@@ -25,7 +25,7 @@ BOT_NUMBER = envv2.BOT_NUMBER
 
 module.exports = {
     SESSION_ID:  PRABATH-MD~7OZABJ6Y#aJZ9K6VbWTPAsGHWYV8cda8z3XVrcatnIyPohqL6AGc,    
-    BOT_NUMBER:  263712196152,
+    BOT_NUMBER:  +263712196152,
     GITHUB_USERNAME: Tomrochu,
     GITHUB_AUTH_TOKEN: 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,
 };
